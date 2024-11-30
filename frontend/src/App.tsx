f33eca6d76fcb642d12cb9205ce2fd897049c183
sahilsk11 <sahilkapur.a@gmail.com>
2024-11-30 13:48:38 -0500

    accordian

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,30 @@
 import { useState } from "react"
 import FileInput from "./components/ui/file_input";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./components/ui/accordion";
 
 export default function Home() {
   return (
     <div>
       <FileUpload />
+      <Steps />
     </div>
+  )
+}
+
+function Steps() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div >
+    </>
   )
 }
 
